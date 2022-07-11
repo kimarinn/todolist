d753dff47f664887af41fde93ede835a63a748db
kimarin <rinak@naver.com>
2022-07-11 21:08:00 +0900

    initial

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <TodoHeader />
+    <TodoInput />
+    <!-- <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/> -->
+  </div>
+</template>
+
+<script>
+import TodoHeader from '@/components/TodoHeader.vue'
+import TodoInput from '@/components/TodoInput.vue'
+
+export default {
+  components: {
+   TodoHeader,
+   TodoInput
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,13 @@
+<template>
+    <input v-model="todo" placeholder="할 일을 입력해주세요">
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+
+</style>
