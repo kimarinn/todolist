faa4ebdeedfbcf77f7f2e8bce5a689d51e6f777d
kimarinn <rinak@naver.com>
2022-09-01 21:20:15 +0900

    0901_삭제동작추가_넘버인덱스로변경

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   <div id="app">
     <TodoHeader />
     <TodoInput @clickBtn="addItem"/>
-    <TodoItem v-for="(i, k) in items" :key="k" :todo="i" />
+    <TodoItem v-for="(i, k) in items" :key="k" :todo="i" @delClick="deleteItem" :index='k'/>
    <!-- <nav>
       <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
@@ -33,13 +33,21 @@ export default {
   },
   methods: {
     addItem(todo){
-      let todoObj = {
+      const todoObj = {
         idx: this.num,
         items: todo
       }
       console.log(todoObj)
       this.items.push(todoObj)
       this.num++
+    },
+    deleteItem (data){
+      console.log('delete', data)
+      console.log('현재', this.items)
+      const todoObj = this.items.filter(items => items.idx !== data)
+      console.log('삭제후', todoObj)
+      //삭제된 항목의 다음 항목들의 idx를 하나씩 줄여야함
+      this.items = todoObj  
     }
   }
 }
